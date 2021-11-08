155f5716f538a214b60d34b968016ade46b72761
fernandobouchet <fernandobouchet@gmail.com>
2021-11-07 23:16:37 -0300

    Added modal style for contact and others improvements

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById("contact_modal");
+var btn = document.getElementById("contact-modal");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "flex";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
