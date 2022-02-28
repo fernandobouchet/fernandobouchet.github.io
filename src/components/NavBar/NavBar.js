9768fd7793ff7cfcb25fd323b60c6081d54820b7
fernandobouchet <fernandobouchet@gmail.com>
2022-02-28 17:24:09 -0300

    Added NavBar component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+function NavBar() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Nav className="me-auto">
+            <Nav.Link href="#About">About</Nav.Link>
+            <Nav.Link href="#Projects">Projects</Nav.Link>
+            <Nav.Link href="#Contact">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
