41057b843ef287c63729524a2aef766f9dc8bb59
fernandobouchet <fernandobouchet@gmail.com>
2022-11-09 23:26:15 -0300

    Themed 'Nav' and some updates to 'About'

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,18 +2,19 @@ export const About = () => {
   return (
     <div
       id="about"
-      className="max-w-3xl mx-auto h-screen flex flex-col gap-11 justify-center items-center p-5"
+      className="max-w-3xl mx-auto h-screen flex flex-col gap-14 justify-center items-center p-5"
     >
       <div>
         <h1 className="font-semibold text-5xl">Hi, I'm Fernando</h1>
       </div>
       <div>
         <p className=" text-xl max-w-2xl text-center font-light">
-          I'm a <span className="text-sky-500">full-stack developer</span>, at
-          present studying a technical degree in programming while I'm learning
-          self-taught MERN stack, currently creating and colaborating in
-          projects to improve my skills. I'm looking for my first professional
-          experience in the IT world.
+          I'm a{" "}
+          <span className="font-normal text-sky-500">full-stack developer</span>
+          , at present studying a technical degree in programming while I'm
+          learning self-taught MERN stack, currently creating and colaborating
+          in projects to improve my skills. I'm looking for my first
+          professional experience in the IT world.
         </p>
       </div>
       <div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,18 +1,26 @@
 export const Nav = () => {
   return (
-    <div className="fixed w-full h-10">
+    <div className="max-w-3xl fixed w-full top-0 left-1/2 -translate-x-1/2 p-5 bg-inherit">
       <ul className="flex flex-row gap-4">
         <li>
-          <a href="#about">About</a>
+          <a href="#about" className="hover:text-stone-400">
+            About
+          </a>
         </li>
         <li>
-          <a href="#skills">Skills</a>
+          <a href="#skills" className="hover:text-stone-400">
+            Skills
+          </a>
         </li>
         <li>
-          <a href="#projects">Projects</a>
+          <a href="#projects" className="hover:text-stone-400">
+            Projects
+          </a>
         </li>
         <li>
-          <a href="#contact">Contact</a>
+          <a href="#contact" className="hover:text-stone-400">
+            Contact
+          </a>
         </li>
       </ul>
     </div>
