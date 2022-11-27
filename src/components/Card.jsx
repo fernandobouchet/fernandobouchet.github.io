095d1792dc8394515bf42128396b4a8e1aa7335f
fernandobouchet <fernandobouchet@gmail.com>
2022-11-27 01:54:48 -0300

    Style and updates to 'Card'

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,18 @@
 import StyledLinkButton from "./StyledLinkButton";
 
 const Card = ({ data }) => {
-  const { title, image, demo, github } = data;
+  const { title, info, image, demo, github } = data;
   return (
     <>
-      <div className="md:text-xl h-max max-w-sm sm:max-w-xl rounded-lg cursor-pointer origin-center translate-x-0 group/item">
-        <img
-          src={image}
-          alt={title}
-          className="group-hover/item:opacity-10 transition duration-700"
-        />
-        <div className="invisible absolute w-full h-full flex flex-col top-0 items-center justify-around group-hover/item:visible">
-          <h2 className="mb-2 font-bold tracking-tight text-gray-900 dark:text-white">
-            {title}
-          </h2>
+      <div className="flex flex-col md:flex-row max-w-sm sm:max-w-3xl items-center rounded-lg cursor-pointer bg-neutral-800 hover:bg-sky-700 transition">
+        <img src={image} alt={title} className="w-82 md:w-96 rounded-lg" />
+        <div className="h-full flex flex-col items-center p-4">
+          <div>
+            <h3 className="mb-2 font-default">{title}</h3>
+            <p className="text-sm md:text-base p-2">{info}</p>
+          </div>
           <div className="flex gap-8">
-            <StyledLinkButton href={demo} text="Demo" />
+            <StyledLinkButton href={demo} text="Preview" />
             <StyledLinkButton href={github} text="Source" />
           </div>
         </div>
--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -2,6 +2,7 @@ export const ProjectsData = [
   {
     id: 0,
     title: "Blog .",
+    info: "Blog website, created a Client for users, a CMS page for admin and an API rest connected to mongoDB database.",
     image:
       "https://github.com/fernandobouchet/TOP_Blog-Client/raw/main/project_preview.png?raw=true",
     demo: "https://blog-top.vercel.app",
@@ -10,6 +11,7 @@ export const ProjectsData = [
   {
     id: 1,
     title: "TOP Series",
+    info: "Used the TMDB API data to displays the most popular series, and allows the user to store the ones they like the most and which they would choose to 'watch later'.",
     image:
       "https://raw.githubusercontent.com/fernandobouchet/favSeries/main/preview.webp",
     demo: "https://favseries.netlify.app",
@@ -19,6 +21,7 @@ export const ProjectsData = [
   {
     id: 2,
     title: "Notebook Store Arg",
+    info: "Simple SPA that displays electronics and allows the user to add them to cart and then 'buy' them.",
     image:
       "https://github.com/fernandobouchet/notebookShop/raw/main/project_preview.jpg?raw=true",
     demo: "https://notebookshop.netlify.app",
@@ -27,6 +30,7 @@ export const ProjectsData = [
   {
     id: 3,
     title: "Quizzical",
+    info: "SPA that show random movies questions using the OPEN TDB API.",
     image:
       "https://github.com/fernandobouchet/quizzical/blob/main/quizzical_preview.jpg?raw=true",
     demo: "https://fernandobouchet.github.io/quizzical",
@@ -35,6 +39,7 @@ export const ProjectsData = [
   {
     id: 4,
     title: "Taskify",
+    info: "Simple TODO list with CRUD functions.",
     image:
       "https://github.com/fernandobouchet/taskify/blob/main/taskify_preview.jpg?raw=true",
     demo: "https://fernandobouchet.github.io/taskify",
