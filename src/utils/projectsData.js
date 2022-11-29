7a942118ffe2409b6b5ade69ee6ab3ef7be5bbb7
fernandobouchet <fernandobouchet@gmail.com>
2022-11-29 00:52:25 -0300

    Added and edited 'projectsData' and added a responsive fix to 'Card'

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -2,7 +2,7 @@ export const ProjectsData = [
   {
     id: 0,
     title: "Blog .",
-    info: "Blog website, created a Client for users, a CMS page for admin and an API rest connected to mongoDB database.",
+    info: "Blog website, created a Client page, a CMS page for admin and an API rest connected to mongoDB database. Used React, React-router, Bootstrap, Express, MongoDB.",
     image:
       "https://github.com/fernandobouchet/TOP_Blog-Client/raw/main/project_preview.png?raw=true",
     demo: "https://blog-top.vercel.app",
@@ -11,35 +11,44 @@ export const ProjectsData = [
   {
     id: 1,
     title: "TOP Series",
-    info: "Used the TMDB API data to displays the most popular series, and allows the user to store the ones they like the most and which they would choose to 'watch later'.",
+    info: "Used the TMDB API data to display the most popular series, and allows the user to save their favs and which they would choose to 'watch later'. Used React, React-router, Chakra-UI, Axios.",
     image:
       "https://raw.githubusercontent.com/fernandobouchet/favSeries/main/preview.webp",
     demo: "https://favseries.netlify.app",
     github: "https://github.com/fernandobouchet/favSeries",
   },
-
   {
     id: 2,
+    title: "Career progress",
+    info: "Website to keep track and record the progress of my university career. Used React, React-router, Bootstrap and Firebase as BasS.",
+    image:
+      "https://github.com/fernandobouchet/ProgresoLicenciaturaInformatica/raw/main/progresolicenciatura_preview.png?raw=true",
+    demo: "https://progreso-licenciatura-informatica.vercel.app",
+    github:
+      "https://github.com/fernandobouchet/ProgresoLicenciaturaInformatica",
+  },
+  {
+    id: 3,
     title: "Notebook Store Arg",
-    info: "Simple SPA that displays electronics and allows the user to add them to cart and then 'buy' them.",
+    info: "Simple SPA that displays electronics and allows the user to add them to cart and then 'buy' them. Used React, Bootstrap.",
     image:
       "https://github.com/fernandobouchet/notebookShop/raw/main/project_preview.jpg?raw=true",
     demo: "https://notebookshop.netlify.app",
     github: "https://github.com/fernandobouchet/notebookShop",
   },
   {
-    id: 3,
+    id: 4,
     title: "Quizzical",
-    info: "SPA that show random movies questions using the OPEN TDB API.",
+    info: "SPA that show random movies questions using the OPEN TDB API. Used React, Vanilla CSS.",
     image:
       "https://github.com/fernandobouchet/quizzical/blob/main/quizzical_preview.jpg?raw=true",
     demo: "https://fernandobouchet.github.io/quizzical",
     github: "https://github.com/fernandobouchet/quizzical",
   },
   {
-    id: 4,
+    id: 5,
     title: "Taskify",
-    info: "Simple TODO list with CRUD functions.",
+    info: "Simple TODO list with CRUD functions. Used React, Tailwindcss.",
     image:
       "https://github.com/fernandobouchet/taskify/blob/main/taskify_preview.jpg?raw=true",
     demo: "https://fernandobouchet.github.io/taskify",
@@ -47,8 +56,9 @@ export const ProjectsData = [
   },
 
   {
-    id: 5,
+    id: 6,
     title: "Pokémon Memory Game",
+    info: "A classic memory game using the 'PokeApi' to fetch the characters of the famous serie. Used React, Vanilla CSS.",
     image:
       "https://github.com/fernandobouchet/memory-game/blob/main/memory-game_preview.jpg?raw=true",
     demo: "https://fernandobouchet.github.io/memory-game",
