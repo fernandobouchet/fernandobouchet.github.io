f70a65f234ffac7db3dac3da79b57015ea8aad29
fernandobouchet <fernandobouchet@gmail.com>
2022-11-29 00:20:45 -0300

    Add a 'load more' button and add the logic to it in 'Projects'

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,9 @@
-const Button = ({ text }) => {
+const Button = ({ text, onClick }) => {
   return (
-    <button className="text-sm md:text-base font-f py-2 px-4 font-medium leading-6 rounded-full bg-sky-800 text-sky-200 hover:bg-sky-700 transition">
+    <button
+      className="text-sm md:text-base font-f py-2 px-4 font-medium leading-6 rounded-full bg-sky-800 text-sky-200 hover:bg-sky-700 transition"
+      onClick={onClick}
+    >
       {text}
     </button>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import { ProjectsData } from "../utils/projectsData";
+import Button from "./Button";
 import Card from "./Card";
 
 const Projects = () => {
+  const [projectsToShow, setprojectsToShow] = useState(3);
+
+  const handleOnClick = () => {
+    setprojectsToShow((prevState) => prevState + 3);
+  };
+
   return (
     <div
       id="projects"
@@ -11,11 +19,14 @@ const Projects = () => {
       <p className="text-sm md:text-base">
         These are some projects I've been working on:
       </p>
-      <div className="pt-10 flex flex-col gap-10">
-        {ProjectsData.slice(0, 3).map((data) => (
+      <div className="pt-10 flex flex-col gap-10 pb-10">
+        {ProjectsData.slice(0, projectsToShow).map((data) => (
           <Card key={data.id} data={data} />
         ))}
       </div>
+      {ProjectsData.length > projectsToShow && (
+        <Button text={"Load more"} onClick={handleOnClick}></Button>
+      )}
     </div>
   );
 };
