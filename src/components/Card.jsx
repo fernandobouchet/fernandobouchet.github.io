367fccfda33503afe1cd6a3f53f60b7003183052
fernandobouchet <fernandobouchet@gmail.com>
2022-12-08 21:26:34 -0300

    Style updates to 'Card' and 'LinkButtonSecondary'

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
-import LinkButtonSecondary from "./LinkButtonSecondary";
+import LinkButtonSecondary from './LinkButtonSecondary';
 
 const Card = ({ data }) => {
   const { title, info, image, demo, github } = data;
   return (
     <>
-      <div className="flex flex-col md:flex-row max-w-sm md:max-w-2xl lg:max-w-3xl items-center rounded-lg cursor-pointer bg-neutral-800 animate-fadeIn">
+      <div className="flex flex-col md:flex-row max-w-sm md:max-w-2xl lg:max-w-3xl items-center rounded-lg bg-neutral-800 animate-fadeIn hover:bg-sky-900 transition">
         <img src={image} alt={title} className="w-82 md:w-96 rounded-lg" />
         <div className="w-full h-full flex flex-col items-center p-3">
           <div>
--- src/components/LinkButtonSecondary.jsx
+++ src/components/LinkButtonSecondary.jsx
@@ -1,12 +1,19 @@
 const LinkButtonSecondary = ({ text, href }) => {
   return (
     <a
-      className="text-sm md:text-base py-2 px-4 leading-6 rounded-full text-sky-200 border border-neutral-400 hover:bg-neutral-700 transition"
+      className="group flex text-sm md:text-base text-sky-300 px-1 py-2 hover:border-b border-sky-200 hover:text-sky-200 transition"
       href={href}
       target="_blank"
       rel="noreferrer"
     >
       {text}
+      <svg
+        className="fill-sky-300 w-[1rem] ml-1 group-hover:stroke-sky-200 transition"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <path d="M18,10.82a1,1,0,0,0-1,1V19a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V8A1,1,0,0,1,5,7h7.18a1,1,0,0,0,0-2H5A3,3,0,0,0,2,8V19a3,3,0,0,0,3,3H16a3,3,0,0,0,3-3V11.82A1,1,0,0,0,18,10.82Zm3.92-8.2a1,1,0,0,0-.54-.54A1,1,0,0,0,21,2H15a1,1,0,0,0,0,2h3.59L8.29,14.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L20,5.41V9a1,1,0,0,0,2,0V3A1,1,0,0,0,21.92,2.62Z" />
+      </svg>
     </a>
   );
 };
