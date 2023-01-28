4d23a5a495415de61789d5ae668fc6472fe9b708
fernandobouchet <fernandobouchet@gmail.com>
2023-01-28 17:36:09 -0300

    Separate some data of projects

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -3,97 +3,103 @@ export const ProjectsData = [
     id: 0,
     title: 'Blog .',
     info_en:
-      'Blog website, created a Client page, a CMS page for admin and an API rest connected to mongoDB database. Used React, React-router, Bootstrap, Express, MongoDB.',
+      'Blog website, created a Client page, a CMS page for admin and an API rest connected to mongoDB database.',
     info_es:
-      'Blog con una página para el "cliente", una página "CMS" para el administrador y una API rest conectada a una base de datos mongoDB. Utilizado React, React-router, Bootstrap, Express, MongoDB.',
+      'Blog con una página para el "cliente", una página "CMS" para el administrador y una API rest conectada a una base de datos mongoDB.',
     image:
       'https://github.com/fernandobouchet/TOP_Blog-Client/raw/main/project_preview.webp',
     demo: 'https://blog-top.vercel.app',
     github: 'https://github.com/fernandobouchet/TOP_Blog-Client',
+    techstack: ['React', 'React-router', 'Bootstrap', 'Express', 'MongoDB'],
   },
   {
     id: 1,
     title: 'TOP Series',
     info_en:
-      "Used the TMDB API data to display the most popular series, and allows the user to save their favs and which they would choose to 'watch later'. Used React, React-router, Chakra-UI, Axios.",
+      "Used the TMDB API data to display the most popular series, and allows the user to save their favs and which they would choose to 'watch later'.",
     info_es:
-      "Se utilizó la API de 'TMDB' para mostrar información de las series más populares, permite al usuario guardar sus series favoritas y las que desearía 'ver más tarde'. Utilizado React, React-router, Chakra-UI, Axios.",
+      "Se utilizó la API de 'TMDB' para mostrar información de las series más populares, permite al usuario guardar sus series favoritas y las que desearía 'ver más tarde'.",
     image:
       'https://raw.githubusercontent.com/fernandobouchet/favSeries/main/preview.webp',
     demo: 'https://favseries.netlify.app',
     github: 'https://github.com/fernandobouchet/favSeries',
+    techstack: ['React', 'React-router', 'Chakra-UI', 'Axios'],
   },
   {
     id: 2,
     title: 'Career progress',
     info_en:
-      'Website to keep track and record the progress of my university career. Used React, React-router, Bootstrap and Firebase as "BasS".',
+      'Website to keep track and record the progress of my university career.',
     info_es:
-      'Página web creada para registrar el progreso de mi carrera universitaria. Utilizado React, React-router, Bootstrap y Firebase como "BasS".',
+      'Página web creada para registrar el progreso de mi carrera universitaria.',
     image:
       'https://github.com/fernandobouchet/ProgresoLicenciaturaInformatica/raw/main/progresolicenciatura_preview.webp',
     demo: 'https://progreso-licenciatura-informatica.vercel.app',
     github:
       'https://github.com/fernandobouchet/ProgresoLicenciaturaInformatica',
+    techstack: ['React', 'React-router', 'Bootstrap', 'Firebase'],
   },
   {
     id: 3,
     title: 'Image Uploader',
     info_en:
-      'Fullstack App that allows to upload, store an image and get his url. Used Typescript, React, Express, MongoDB.',
+      'Fullstack App that allows to upload, store an image and get his url.',
     info_es:
-      'Aplicación fullstack que permite subir, almacenar una imagen y obtener su url. Utilizado Typescript, React, Express, MongoDB.',
+      'Aplicación fullstack que permite subir, almacenar una imagen y obtener su url.',
     image:
       'https://raw.githubusercontent.com/fernandobouchet/image-uploader/main/project_preview.webp',
     demo: 'http://image-uploader-beryl.vercel.app',
     github: 'https://github.com/fernandobouchet/image-uploader',
+    techstack: ['React', 'Typescript', 'Express', 'MongoDB'],
   },
   {
     id: 4,
     title: 'Notebook Store Arg',
     info_en:
-      "Simple SPA that displays electronics and allows the user to add them to cart and then 'buy' them. Used React, Bootstrap.",
+      "Simple SPA that displays electronics and allows the user to add them to cart and then 'buy' them.",
     info_es:
-      "SPA que muestra productos electrónicos y permite al usuario agregar productos al carrito y 'comprarlos'. Utilizado React, Bootstrap.",
+      "SPA que muestra productos electrónicos y permite al usuario agregar productos al carrito y 'comprarlos'.",
     image:
       'https://github.com/fernandobouchet/notebookShop/raw/main/project_preview.jpg?raw=true',
     demo: 'https://notebookshop.netlify.app',
     github: 'https://github.com/fernandobouchet/notebookShop',
+    techstack: ['React', 'Bootstrap'],
   },
   {
     id: 5,
     title: 'Quizzical',
-    info_en:
-      'SPA that show random movies questions using the OPEN TDB API. Used React, Vanilla CSS.',
+    info_en: 'SPA that show random movies questions using the OPEN TDB API.',
     info_es:
-      'SPA que muestra preguntas aleatorias de peliculas usando la API de "OPEN TDB". Utilizado React, Vanilla CSS.',
+      'SPA que muestra preguntas aleatorias de peliculas usando la API de "OPEN TDB".',
     image:
       'https://github.com/fernandobouchet/quizzical/blob/main/quizzical_preview.jpg?raw=true',
     demo: 'https://fernandobouchet.github.io/quizzical',
     github: 'https://github.com/fernandobouchet/quizzical',
+    techstack: ['React', 'CSS'],
   },
   {
     id: 6,
     title: 'Taskify',
-    info_en: 'Simple TODO list with CRUD functions. Used React, Tailwindcss.',
-    info_es:
-      'Simple lista de "cosas para hacer", con funciones "CRUD". Utilizado React, Tailwindcss.',
+    info_en: 'Simple TODO list with CRUD functions.',
+    info_es: 'Simple lista de "cosas para hacer", con funciones "CRUD".',
     image:
       'https://github.com/fernandobouchet/taskify/blob/main/taskify_preview.jpg?raw=true',
     demo: 'https://fernandobouchet.github.io/taskify',
     github: 'https://github.com/fernandobouchet/taskify',
+    techstack: ['React', 'Tailwind'],
   },
 
   {
     id: 7,
     title: 'Pokémon Memory Game',
     info_en:
-      "A classic memory game using the 'PokeApi' to fetch the characters of the famous serie. Used React, Vanilla CSS.",
+      "A classic memory game using the 'PokeApi' to fetch the characters of the famous serie.",
     info_es:
-      "Clásico juego de memorio utilizando la API de 'PokeApi' para obtener los personajes de la famosa serie. Utilizado React, Vanilla CSS.",
+      "Clásico juego de memorio utilizando la API de 'PokeApi' para obtener los personajes de la famosa serie.",
     image:
       'https://github.com/fernandobouchet/memory-game/blob/main/memory-game_preview.jpg?raw=true',
     demo: 'https://fernandobouchet.github.io/memory-game',
     github: 'https://github.com/fernandobouchet/memory-game',
+    techstack: ['React', 'CSS'],
   },
 ];
