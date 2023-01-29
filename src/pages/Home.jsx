2667be2ab05dee405faa1bfbc9a95cba66534bf3
fernandobouchet <fernandobouchet@gmail.com>
2023-01-29 16:57:19 -0300

    Theme update for the Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,13 @@ import Projects from '../components/Projects';
 
 const Home = () => {
   return (
-    <>
+    <div className="bg-light-background text-light-font dark:bg-dark-background dark:text-dark-font">
       <NavBar />
       <About />
       <Projects />
       <Contact />
       <Footer />
-    </>
+    </div>
   );
 };
 export default Home;
