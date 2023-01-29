af0e10f4342280aade782888641478c8931b3e83
fernandobouchet <fernandobouchet@gmail.com>
2023-01-29 16:57:40 -0300

    Theme update to 'NavBar'

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,21 +6,30 @@ const NavBar = () => {
   const { texts, handleChangeLanguage, language } = useContext(LanguageContext);
   return (
     <>
-      <div className="z-10 h-14 fixed w-full top-0 left-1/2 -translate-x-1/2 p-5 bg-neutral-100 dark:bg-neutral-800 animate-fadeIn transition">
+      <div className="z-10 h-14 fixed w-full top-0 left-1/2 -translate-x-1/2 p-5 text-light-fontSurface dark:text-dark-fontSurface bg-light-surface dark:bg-dark-surface animate-fadeIn transition">
         <nav className="flex max-w-5xl m-auto">
           <ul className="flex flex-row gap-4 text-xs md:text-sm font-medium items-center">
             <li>
-              <a href="#about" className="hover:text-stone-400">
+              <a
+                href="#about"
+                className="hover:text-light-font dark:hover:text-dark-font"
+              >
                 {texts.navbar.about}
               </a>
             </li>
             <li>
-              <a href="#projects" className="hover:text-stone-400">
+              <a
+                href="#projects"
+                className="hover:text-light-font dark:hover:text-dark-font"
+              >
                 {texts.navbar.projects}
               </a>
             </li>
             <li>
-              <a href="#contact" className="hover:text-stone-400">
+              <a
+                href="#contact"
+                className="hover:text-light-font dark:hover:text-dark-font"
+              >
                 {texts.navbar.contact}
               </a>
             </li>
@@ -29,7 +38,7 @@ const NavBar = () => {
             <li>
               <button
                 onClick={() => handleChangeLanguage()}
-                className="hover:text-stone-400"
+                className="hover:text-light-font dark:hover:text-dark-font"
               >
                 {language}
               </button>
