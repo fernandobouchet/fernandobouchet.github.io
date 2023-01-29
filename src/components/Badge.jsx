03bcc8e22d99c6ad0bda03d6fd20c3155b4d0108
fernandobouchet <fernandobouchet@gmail.com>
2023-01-29 17:38:29 -0300

    Style updates to 'Bagde'

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,7 +1,7 @@
 const Badge = ({ text }) => {
   return (
     <>
-      <span className="m-1 rounded-md text-xs bg-light-surface dark:bg-dark-surface text-light-fontSurface dark:text-dark-fontSurface md:text-sm font-light mr-2 px-2.5 py-0.5">
+      <span className="m-1 rounded-lg text-xs bg-light-surface dark:bg-dark-surface text-light-fontSurface dark:text-dark-fontSurface md:text-sm font-light mr-2 px-2.5 pt-1.5 pb-1">
         {text}
       </span>
     </>
