ba686b6e092cd7377fbd47d493ff82b90f1acb65
fernandobouchet <fernandobouchet@gmail.com>
2023-04-04 23:36:23 -0300

    Remove font load from html, import it from the package

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "@fontsource/roboto-flex";
 import { LanguageProvider } from './context/LanguageContext';
 import './index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
