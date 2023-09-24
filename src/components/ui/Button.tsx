a015494a15a77f0eb50744bf56b88b759541af48
fernandobouchet <fernandobouchet@gmail.com>
2023-09-24 00:08:36 -0300

    feat: add 'button' component with variants

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,24 @@
+import { useContext } from 'react';
+import { ThemeContext } from '../../context/ThemeContext';
+import style from '../../styles/components/ui/Button.module.scss';
+
+interface Props {
+  className?: string;
+  text: string;
+}
+
+const Button = ({ className, text }: Props) => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <button
+      className={`${style.button} ${
+        className && style[className + '-' + theme]
+      }`}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
