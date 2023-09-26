f4fcfe27fc568fd718acbe58820dc944b2114e21
fernandobouchet <fernandobouchet@gmail.com>
2023-09-25 23:21:39 -0300

    feat: set absolute paths for most commons imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      {
+        find: '@components',
+        replacement: path.resolve(path.join(__dirname, '/src/components')),
+      },
+      {
+        find: '@styles',
+        replacement: path.resolve(path.join(__dirname, '/src/styles')),
+      },
+      {
+        find: '@context',
+        replacement: path.resolve(path.join(__dirname, '/src/context')),
+      },
+    ],
+  },
+});
