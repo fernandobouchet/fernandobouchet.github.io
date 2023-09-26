00b720a68304b71ce022c9b9410e924e527e9217
fernandobouchet <fernandobouchet@gmail.com>
2023-09-26 00:01:21 -0300

    feat: add 'Footer' and update 'underline-effect' mixin to be more customizable

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,25 @@
+import style from '@styles/components/Footer.module.scss';
+import { useContext } from 'react';
+import { ThemeContext } from '@context/ThemeContext';
+import { Github } from '@components/icons';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <div className={style.footer__container}>
+      <a
+        className={`${style.footer__link} ${style[theme]}`}
+        href="https://github.com/fernandobouchet"
+        target="_blank"
+      >
+        <Github className={style.footer__link__icon} />{' '}
+        <span className={style.footer__link__text}>
+          Fernando Bouchet - {new Date().getFullYear()}
+        </span>
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
