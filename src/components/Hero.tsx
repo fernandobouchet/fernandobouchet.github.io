2f45b224aa07f9b302415e93f03821c9e1ad8f87
fernandobouchet <fernandobouchet@gmail.com>
2023-10-20 00:50:14 -0300

    feat: add 'Profile' picture in 'Hero'

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,10 @@
-import style from '@styles/components/Hero.module.scss';
-import { ThemeContext } from '@context/ThemeContext';
-import BadgesContainer from '@components/BadgesContainer';
-import LinkButton from './ui/LinkButton';
-import useIntersection from '../hooks/useIntersection';
-import { useContext, useRef } from 'react';
-import { LanguageContext } from '@context/LanguageContext';
+import style from "@styles/components/Hero.module.scss";
+import { ThemeContext } from "@context/ThemeContext";
+import BadgesContainer from "@components/BadgesContainer";
+import LinkButton from "./ui/LinkButton";
+import useIntersection from "../hooks/useIntersection";
+import { useContext, useRef } from "react";
+import { LanguageContext } from "@context/LanguageContext";
 
 const Hero = () => {
   const { theme } = useContext(ThemeContext);
@@ -18,26 +18,31 @@ const Hero = () => {
     <div
       id="about"
       className={`fade-in section__full-screen ${
-        isIntersecting ? 'fade-in-active' : ''
+        isIntersecting ? "fade-in-active" : ""
       }`}
       ref={ref}
     >
       <div className={style.hero__container}>
+        <img
+          className={style.hero__img}
+          src="/public/Profile.png"
+          alt="Fernando Bouchet profile picture"
+        ></img>
         <h1 className={style.hero__title}>{texts.about.hero}</h1>
         <p className={style.hero__text}>
           {texts.about.p1}
           <span className={`${style.hero__text__span} ${style[theme]}`}>
             {texts.about.span1}
           </span>
-          {texts.about.p2}{' '}
+          {texts.about.p2}{" "}
           <span className={`${style.hero__text__span} ${style[theme]}`}>
             {texts.about.span2}
           </span>
-          {texts.about.p3}{' '}
+          {texts.about.p3}{" "}
           <span className={`${style.hero__text__span} ${style[theme]}`}>
             {texts.about.span3}
-          </span>{' '}
-          {texts.about.p4}{' '}
+          </span>{" "}
+          {texts.about.p4}{" "}
         </p>
         <BadgesContainer />
         <LinkButton
