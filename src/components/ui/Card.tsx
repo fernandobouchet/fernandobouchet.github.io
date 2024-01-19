b7e5aa54aabf83056786690604b597a82e40e654
fernandobouchet <fernandobouchet@gmail.com>
2024-01-18 21:03:53 -0300

    feat: update 'Card' component and some style updates

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,11 +1,10 @@
-import style from "@styles/components/ui/Card.module.scss";
-import { useContext } from "react";
-import { ThemeContext } from "@context/ThemeContext";
-import Badge from "./Badge";
-import { Github } from "@components/icons";
-import LinkButton from "./LinkButton";
-import useImageIsLoaded from "@hooks/useImageIsLoaded";
-import { LanguageContext } from "@context/LanguageContext";
+import style from '@styles/components/ui/Card.module.scss';
+import { useContext } from 'react';
+import { ThemeContext } from '@context/ThemeContext';
+import Badge from './Badge';
+import { Github, Link } from '@components/icons';
+import useImageIsLoaded from '@hooks/useImageIsLoaded';
+import { LanguageContext } from '@context/LanguageContext';
 
 interface Props {
   data: {
@@ -22,7 +21,7 @@ interface Props {
 
 const Card = ({ data }: Props) => {
   const { theme } = useContext(ThemeContext);
-  const { texts, language } = useContext(LanguageContext);
+  const { language } = useContext(LanguageContext);
 
   const isImageLoaded = useImageIsLoaded(data.image);
 
@@ -30,42 +29,43 @@ const Card = ({ data }: Props) => {
     <div className={`fadeIn ${style.card}  ${style[theme]}`}>
       <div className={style.card__image__wrapper}>
         {isImageLoaded ? (
-          <div
-            className={`${style.card__image__wrapper} ${style.card__image}`}
-            style={{ backgroundImage: `url(${data.image})` }}
-          />
+          <a href={data.demo} target="_blank">
+            <div
+              className={`${style.card__image__wrapper} ${style.card__image}`}
+              style={{ backgroundImage: `url(${data.image})` }}
+            />
+          </a>
         ) : (
           <div
             className={`pulse ${style.card__image__wrapper}`}
-            style={{ background: "#373a40" }}
+            style={{ background: '#373a40' }}
           ></div>
         )}
       </div>
       <div className={style.card__content}>
         <div className={style.card__content__title__wrapper}>
           <h3 className={style.card__content__title}>{data.title}</h3>
-          <a href={data.github} target="_blank">
-            <Github />
-          </a>
+          <div className={style.card__links__wrapper}>
+            <a href={data.demo} target="_blank">
+              <Link />
+            </a>
+            <a href={data.github} target="_blank">
+              <Github />
+            </a>
+          </div>
         </div>
         <p className={style.card__content__text}>
-          {language === "EN" ? data.info_en : data.info_es}
+          {language === 'EN' ? data.info_en : data.info_es}
         </p>
         <div className={style.card__content__bagde__wrapper}>
           {data.techstack.map((stack) => (
             <Badge
               key={stack}
-              className={"card__content__badge"}
+              className={'card__content__badge'}
               text={`#${stack}`}
             />
           ))}
         </div>
-        <LinkButton
-          className="secondary"
-          text={texts.cardProjects.preview}
-          link={data.demo}
-          description={`Link to deployment of the ${data.title} website`}
-        />
       </div>
     </div>
   );
