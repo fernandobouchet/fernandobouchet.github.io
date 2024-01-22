1af97664ccac6b195d67d66c2be8b484ea380994
fernandobouchet <fernandobouchet@gmail.com>
2024-01-21 22:24:20 -0300

    feat: update 'Footer' style and 'accesibility' fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,11 @@
-import style from "@styles/components/Footer.module.scss";
-import { useContext } from "react";
-import { ThemeContext } from "@context/ThemeContext";
-import { Github } from "@components/icons";
+import style from '@styles/components/Footer.module.scss';
+import { useContext } from 'react';
+import { ThemeContext } from '@context/ThemeContext';
+import { Github } from '@components/icons';
 
 const Footer = () => {
   const { theme } = useContext(ThemeContext);
+  const text = `Fernando Bouchet - ${new Date().getFullYear()}`;
 
   return (
     <div className={style.footer__container}>
@@ -12,12 +13,10 @@ const Footer = () => {
         className={`${style.footer__link} ${style[theme]}`}
         href="https://github.com/fernandobouchet"
         target="_blank"
-        aria-label="Fernando Bouchet github link"
+        aria-label={`${text} github link`}
       >
-        <Github className={style.footer__link__icon} />{" "}
-        <span className={style.footer__link__text}>
-          Fernando Bouchet - {new Date().getFullYear()}
-        </span>
+        <Github className={style.footer__link__icon} />
+        <span className={style.footer__link__text}>{text}</span>
       </a>
     </div>
   );
