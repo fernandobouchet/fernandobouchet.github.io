1f5ae0764cede83cfb53506d0e17a77406c4a364
fernandobouchet <fernandobouchet@gmail.com>
2024-02-05 23:35:26 -0300

    feat: add 'million' dependency and configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import million from 'million/compiler';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [million.vite({ auto: true }), react()],
   root: './',
   build: {
     outDir: './build',
