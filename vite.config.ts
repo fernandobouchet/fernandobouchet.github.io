a4802a992213df156b41806412d2582a0f17e1de
fernandobouchet <fernandobouchet@gmail.com>
2024-02-12 17:19:21 -0300

    revert: 7904919aabaeb7a7fd429417181fb194e02b87be

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import million from 'million/compiler';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
+  plugins: [react()],
   root: './',
   build: {
     outDir: './build',
