b3253688ea74bcf37067b76fb66274622a819605
fernandobouchet <fernandobouchet@gmail.com>
2024-04-23 00:49:56 -0300

    feat: add a 'background' component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { Background } from "@/components/ui/background";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Background />
           {children}
         </ThemeProvider>
       </body>
---
+++ components/ui/background.tsx
@@ -0,0 +1,9 @@
+const Background = () => {
+  return (
+    <div className="fixed left-0 top-0 -z-10 h-full w-full">
+      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#67e8f9,transparent)] dark:bg-[radial-gradient(circle_500px_at_50%_200px,#083344,transparent)]" />
+    </div>
+  );
+};
+
+export { Background };
