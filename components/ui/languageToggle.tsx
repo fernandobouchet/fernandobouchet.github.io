36c7d2fef1e4a354e8c8b184abb1a029bb3c86fe
fernandobouchet <fernandobouchet@gmail.com>
2024-04-23 23:20:00 -0300

    feat: add 'language toggle' component

---
+++ components/ui/languageToggle.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { LanguageContext } from "@/context/languageContext";
+import { useContext } from "react";
+
+const LanguageToggle = () => {
+  const { language, handleChangeLanguage } = useContext(LanguageContext);
+
+  return (
+    <button
+      className="text-sm md:text-base font-semibold text-foreground"
+      onClick={() => handleChangeLanguage()}
+    >
+      {language === "EN" ? "ES" : "EN"}
+    </button>
+  );
+};
+
+export { LanguageToggle };
