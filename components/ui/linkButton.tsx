51948c8e1ef08adab840f1541e0eba020ccecd24
fernandobouchet <fernandobouchet@gmail.com>
2024-04-26 20:45:35 -0300

    feat: add 'about' components

---
+++ components/ui/linkButton.tsx
@@ -0,0 +1,23 @@
+import { IconArrowRight } from "@/components/icons";
+
+interface Props {
+  link: string;
+  text: string;
+}
+
+const LinkButton = ({ link, text }: Props) => {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      className="group relative inline-flex h-12 items-center text-base md:text-lg justify-center overflow-hidden rounded-full bg-primary px-6 text-primary-foreground font-semibold my-2"
+    >
+      <span>{text}</span>
+      <div className="w-0 translate-x-[100%] pl-0 opacity-0 transition-all duration-200 group-hover:w-5 group-hover:translate-x-0 group-hover:pl-2 group-hover:opacity-100">
+        <IconArrowRight className="h-5 w-5" />
+      </div>
+    </a>
+  );
+};
+
+export { LinkButton };
