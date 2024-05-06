441e5d0ee31ec0e51549337c8d76fbf3d0b4b168
fernandobouchet <fernandobouchet@gmail.com>
2024-05-05 23:05:41 -0300

    feat: update 'Footer'

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,7 +1,21 @@
-const Footer = () => {
-  const text = `Fernando Bouchet - ${new Date().getFullYear()}`;
+import { IconGithub } from "@/components/icons";
 
-  return <footer className="flex justify-center text-sm">{text}</footer>;
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="flex justify-center text-xs py-1">
+      <a
+        href="https://github.com/fernandobouchet/fernandobouchet.github.io"
+        target="_blank"
+        className="flex items-center gap-1 px-1"
+      >
+        © Fernando Bouchet
+        <IconGithub className="w-3 h-auto" />
+      </a>
+      {"  "} - {year}
+    </footer>
+  );
 };
 
 export { Footer };
