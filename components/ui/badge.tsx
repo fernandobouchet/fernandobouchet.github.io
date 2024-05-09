b800bc60c1ad68c06445da2e210fb9b0b842bdc9
fernandobouchet <fernandobouchet@gmail.com>
2024-05-08 23:59:26 -0300

    feat: update 'badge' component style

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -6,7 +6,7 @@ interface Props {
 const Badge = ({ text, className }: Props) => {
   return (
     <div
-      className={`flex min-w-fit items-center gap-2 px-2 py-1 bg-tertiary text-tertiary-foreground rounded-full font-medium text-xs md:text-sm ${className}`}
+      className={`flex min-w-fit items-center gap-2 px-2 py-1 bg-tertiary text-tertiary-foreground rounded-full font-medium text-xs md:text-sm whitespace-nowrap ${className}`}
     >
       <span>{text}</span>
     </div>
