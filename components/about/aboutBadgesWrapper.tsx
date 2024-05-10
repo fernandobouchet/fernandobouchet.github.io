c0886a50c96ce968989d951d482eb5701857f698
fernandobouchet <fernandobouchet@gmail.com>
2024-05-09 23:13:37 -0300

    feat: add corresponding icon to about badges

--- components/about/aboutBadgesWrapper.tsx
+++ components/about/aboutBadgesWrapper.tsx
@@ -1,17 +1,30 @@
 import { Badge } from "@/components/ui/badge";
+import {
+  IconCss,
+  IconExpress,
+  IconHtml,
+  IconJs,
+  IconMongodb,
+  IconMysql,
+  IconNextjs,
+  IconNode,
+  IconPostgresql,
+  IconReact,
+  IconTs,
+} from "../icons";
 
 const Technologies = [
-  { id: 0, title: "HTML" },
-  { id: 1, title: "CSS" },
-  { id: 2, title: "JAVASCRIPT" },
-  { id: 3, title: "TYPESCRIPT" },
-  { id: 4, title: "REACT" },
-  { id: 5, title: "NEXT JS" },
-  { id: 6, title: "NODE JS" },
-  { id: 7, title: "EXPRESS" },
-  { id: 8, title: "MONGO DB" },
-  { id: 9, title: "POSTGRESQL" },
-  { id: 10, title: "MYSQL" },
+  { id: 0, title: "HTML", icon: <IconHtml /> },
+  { id: 1, title: "CSS", icon: <IconCss /> },
+  { id: 2, title: "JAVASCRIPT", icon: <IconJs /> },
+  { id: 3, title: "TYPESCRIPT", icon: <IconTs /> },
+  { id: 4, title: "REACT", icon: <IconReact /> },
+  { id: 5, title: "NEXT JS", icon: <IconNextjs /> },
+  { id: 6, title: "NODE JS", icon: <IconNode /> },
+  { id: 7, title: "EXPRESS", icon: <IconExpress /> },
+  { id: 8, title: "MONGO DB", icon: <IconMongodb /> },
+  { id: 9, title: "POSTGRESQL", icon: <IconPostgresql /> },
+  { id: 10, title: "MYSQL", icon: <IconMysql /> },
 ];
 
 const AboutBadgesWrapper = () => {
@@ -19,7 +32,11 @@ const AboutBadgesWrapper = () => {
     <div className="max-w-xs md:max-w-md lg:max-w-lg inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)] py-4">
       <ul className="flex items-center justify-center md:justify-start animate-infinite-scroll">
         {Technologies.map((item) => (
-          <li className="mx-4" key={item.id}>
+          <li
+            className="mx-4 flex flex-col items-center [&_svg]:w-6 [&_svg]:h-6 [&_svg]:md:w-8 [&_svg]:md:h-8 [&_svg]:!fill-tertiary-foreground"
+            key={item.id}
+          >
+            {item.icon}
             <Badge key={item.id} text={item.title} className="bg-transparent" />
           </li>
         ))}
@@ -29,7 +46,11 @@ const AboutBadgesWrapper = () => {
         aria-hidden="true"
       >
         {Technologies.map((item) => (
-          <li className="mx-4" key={item.id}>
+          <li
+            className="mx-4 flex flex-col items-center [&_svg]:w-6 [&_svg]:h-6 [&_svg]:md:w-8 [&_svg]:md:h-8 [&_svg]:!fill-tertiary-foreground"
+            key={item.id}
+          >
+            {item.icon}
             <Badge key={item.id} text={item.title} className="bg-transparent" />
           </li>
         ))}
