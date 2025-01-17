41668c34e55ed277919f06d5018ca4fac67b3fcf
fernandobouchet <fernandobouchet@gmail.com>
2025-01-17 19:32:19 -0300

    feat: update 'section-container'

--- components/ui/section-container.tsx
+++ components/ui/section-container.tsx
@@ -11,10 +11,11 @@ const SectionContainer = ({ sectionId, children }: Props) => {
     <section id={sectionId} className="section-container">
       <InView
         variants={{
-          hidden: { opacity: 0, y: 100, filter: "blur(10px)" },
+          hidden: { opacity: 0, y: 10, filter: "blur(10px)" },
           visible: { opacity: 1, y: 0, filter: "blur(0px)" },
         }}
-        viewOptions={{ margin: "0px 0px -200px 0px" }}
+        viewOptions={{ margin: "40px 0px -150px 0px" }}
+        transition={{ duration: 0.5 }}
       >
         {children}
       </InView>
