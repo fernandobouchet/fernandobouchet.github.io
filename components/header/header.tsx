a8335ae4cbbb67722d1254b6b048591bba1fa1be
fernandobouchet <fernandobouchet@gmail.com>
2025-01-21 20:18:32 -0300

    fix: fixed 'header' style

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -2,7 +2,7 @@ import { Nav } from "./nav";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 right-0 w-full flex backdrop-blur h-12 z-50 p-3">
+    <header className="fixed top-0 right-0 w-full flex backdrop-blur h-12 z-50 p-3">
       <Nav />
     </header>
   );
