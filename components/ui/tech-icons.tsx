7f7ef9d3311e4d0218ef8f58568fc2cf2337955f
fernandobouchet <fernandobouchet@gmail.com>
2025-01-21 20:39:37 -0300

    feat: add 'tech-icons'

---
+++ components/ui/tech-icons.tsx
@@ -0,0 +1,67 @@
+import {
+  SiBootstrap,
+  SiChakraui,
+  SiCss3,
+  SiExpress,
+  SiFirebase,
+  SiHtml5,
+  SiJavascript,
+  SiMongodb,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiPostgresql,
+  SiReact,
+  SiReactquery,
+  SiReactrouter,
+  SiStyledcomponents,
+  SiSupabase,
+  SiTailwindcss,
+  SiTypescript,
+  SiRedux,
+  SiShadcnui,
+  SiPrisma,
+  SiNextui,
+} from "react-icons/si";
+import { GrMysql } from "react-icons/gr";
+import { TbApi } from "react-icons/tb";
+import { TfiWorld } from "react-icons/tfi";
+import { FaJava } from "react-icons/fa";
+import { BiLogoSpringBoot } from "react-icons/bi";
+
+const TechIcons = {
+  html: { title: "HTML", icon: <SiHtml5 /> },
+  css: { title: "CSS", icon: <SiCss3 /> },
+  javascript: { title: "JavaScript", icon: <SiJavascript /> },
+  typescript: { title: "TypeScript", icon: <SiTypescript /> },
+  react: { title: "React", icon: <SiReact /> },
+  reactQuery: { title: "React Query", icon: <SiReactquery /> },
+  reactRouter: { title: "React Router", icon: <SiReactrouter /> },
+  nextjs: { title: "Next.js", icon: <SiNextdotjs /> },
+  nodejs: { title: "Node.js", icon: <SiNodedotjs /> },
+  express: { title: "Express", icon: <SiExpress /> },
+  mongodb: { title: "MongoDB", icon: <SiMongodb /> },
+  postgresql: { title: "PostgreSQL", icon: <SiPostgresql /> },
+  mysql: { title: "MySQL", icon: <GrMysql /> },
+  supabase: {
+    title: "Supabase",
+    icon: <SiSupabase />,
+  },
+  firebase: { title: "Firebase", icon: <SiFirebase /> },
+  bootstrap: { title: "Bootstrap", icon: <SiBootstrap /> },
+  styledComponents: {
+    title: "Styled Components",
+    icon: <SiStyledcomponents />,
+  },
+  chakraUi: { title: "Chakra UI", icon: <SiChakraui /> },
+  tailwind: { title: "Tailwind CSS", icon: <SiTailwindcss /> },
+  api: { title: "API", icon: <TbApi /> },
+  redux: { title: "Redux", icon: <SiRedux /> },
+  shadcnui: { title: "Shadcn/UI", icon: <SiShadcnui /> },
+  prisma: { title: "Prisma", icon: <SiPrisma /> },
+  nextUI: { title: "Next UI", icon: <SiNextui /> },
+  i18n: { title: "Next-intl", icon: <TfiWorld /> },
+  java: { title: "Java", icon: <FaJava /> },
+  springboot: { title: "Springboot", icon: <BiLogoSpringBoot /> },
+};
+
+export { TechIcons };
