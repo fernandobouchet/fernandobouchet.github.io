cf805aea37a436c37a41a9de1d61c1328242f863
fernandobouchet <fernandobouchet@gmail.com>
2025-01-20 23:38:52 -0300

    feat: update 'next.config.ts'

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
