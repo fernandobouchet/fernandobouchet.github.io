0c5748539543c10b4cabd60117851d640c8aed89
fernandobouchet <fernandobouchet@gmail.com>
2025-01-20 21:27:53 -0300

    feat: update 'in-view' and 'section-container' components

--- components/ui/in-view.tsx
+++ components/ui/in-view.tsx
@@ -17,6 +17,7 @@ export type InViewProps = {
   transition?: Transition;
   viewOptions?: UseInViewOptions;
   as?: React.ElementType;
+  className?: string;
 };
 
 const defaultVariants = {
@@ -29,6 +30,7 @@ export function InView({
   variants = defaultVariants,
   transition,
   viewOptions,
+  className,
   as = "div",
 }: InViewProps) {
   const ref = useRef(null);
@@ -43,6 +45,7 @@ export function InView({
       animate={isInView ? "visible" : "hidden"}
       variants={variants}
       transition={transition}
+      className={className}
     >
       {children}
     </MotionComponent>
--- components/ui/section-container.tsx
+++ components/ui/section-container.tsx
@@ -8,14 +8,15 @@ interface Props {
 
 const SectionContainer = ({ sectionId, children }: Props) => {
   return (
-    <section id={sectionId} className="section-container">
+    <section id={sectionId}>
       <InView
         variants={{
           hidden: { opacity: 0, y: 10, filter: "blur(10px)" },
           visible: { opacity: 1, y: 0, filter: "blur(0px)" },
         }}
-        viewOptions={{ margin: "40px 0px -150px 0px" }}
+        viewOptions={{ margin: "0px 0px -200px 0px" }}
         transition={{ duration: 0.5 }}
+        className="section-container"
       >
         {children}
       </InView>
