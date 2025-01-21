9ff2bf598289cf4c0f3bf9f20637f38205ca1710
fernandobouchet <fernandobouchet@gmail.com>
2025-01-20 23:11:02 -0300

    feat: add 'theme' colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,26 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "var(--primary)",
+          foreground: "var(--primary-foreground)",
+        },
+        secondary: {
+          DEFAULT: "var(--secondary)",
+          foreground: "var(--secondary-foreground)",
+        },
+        accent: {
+          DEFAULT: "var(--accent)",
+          foreground: "var(--accent-foreground)",
+        },
+        muted: {
+          DEFAULT: "var(--muted)",
+          foreground: "var(--muted-foreground)",
+        },
+        card: {
+          DEFAULT: "var(--card)",
+          foreground: "var(--card-foreground)",
+        },
       },
     },
   },
