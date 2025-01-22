0be1b01860a3108328843defd29b81a763d41861
fernandobouchet <fernandobouchet@gmail.com>
2025-01-21 23:56:41 -0300

    feat: add 'button' component

---
+++ components/ui/button.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ReactNode } from "react";
+
+interface Props {
+  className?: string;
+  onClick?: () => void;
+  children: ReactNode;
+}
+
+const Button = ({ className, children, onClick }: Props) => {
+  return (
+    <button
+      className={`group relative inline-flex  h-12 items-center text-base lg:text-lg justify-center overflow-hidden rounded-full bg-primary px-6 text-primary-foreground font-semibold my-2 *:z-10 ${className}`}
+      onClick={onClick ? onClick : undefined}
+    >
+      <span className="absolute h-0 w-0 rounded-full bg-secondary transition-all duration-300 group-hover:h-full group-hover:w-full"></span>
+      <span>{children}</span>
+    </button>
+  );
+};
+
+export { Button };
