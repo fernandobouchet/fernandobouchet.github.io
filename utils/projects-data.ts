b7d3058d8ae3cb9e2b19f63c2e611787d46e0f89
fernandobouchet <fernandobouchet@gmail.com>
2025-01-21 23:44:12 -0300

    feat: add 'projects-data'

---
+++ utils/projects-data.ts
@@ -0,0 +1,241 @@
+export const ProjectsData = [
+  {
+    id: 0,
+    title: "Pokémon Memory Game",
+    info_es:
+      "Clásico juego de memoria utilizando la API de 'PokeApi' para obtener los personajes de la famosa serie.",
+    info_en:
+      "Classic memory game using the 'PokeApi' API to obtain the characters from the famous series.",
+    image:
+      "https://github.com/fernandobouchet/memory-game/blob/main/memory-game_preview.jpg?raw=true",
+    demo: "https://fernandobouchet.github.io/memory-game",
+    github: "https://github.com/fernandobouchet/memory-game",
+    techstack: ["React", "API"],
+  },
+  {
+    id: 1,
+    title: "Taskify",
+    info_es: 'Simple lista de "cosas para hacer", con funciones "CRUD".',
+    info_en: 'Simple "to do" list, with "CRUD" functions.',
+    image:
+      "https://github.com/fernandobouchet/taskify/blob/main/taskify_preview.jpg?raw=true",
+    demo: "https://fernandobouchet.github.io/taskify",
+    github: "https://github.com/fernandobouchet/taskify",
+    techstack: ["React", "Tailwind CSS", "TypeScript"],
+  },
+  {
+    id: 2,
+    title: "Random Quotes",
+    info_es: "Aplicación que muestra frases conocidas de manera aleatoria.",
+    info_en: "Application that shows known phrases randomly.",
+    image:
+      "https://github.com/fernandobouchet/random-quotes/raw/main/random-quotes_preview.webp?raw=true",
+    demo: "https://fernandobouchet.github.io/random-quotes",
+    github: "https://github.com/fernandobouchet/random-quotes",
+    techstack: ["React", "Styled Components", "API"],
+  },
+  {
+    id: 3,
+    title: "Quizzical",
+    info_es:
+      'SPA que muestra preguntas aleatorias de peliculas usando la API de "OPEN TDB".',
+    info_en: 'SPA that shows random movie questions using the "OPEN TDB" API.',
+    image:
+      "https://github.com/fernandobouchet/quizzical/blob/main/quizzical_preview.jpg?raw=true",
+    demo: "https://fernandobouchet.github.io/quizzical",
+    github: "https://github.com/fernandobouchet/quizzical",
+    techstack: ["React", "API"],
+  },
+  {
+    id: 4,
+    title: "Notebook Store Arg",
+    info_es:
+      "SPA que muestra productos electrónicos y permite al usuario agregar productos al carrito y 'comprarlos'.",
+    info_en:
+      "SPA that displays electronic products and allows the user to add products to the cart and 'purchase' them.",
+    image:
+      "https://github.com/fernandobouchet/notebookShop/raw/main/project_preview.jpg?raw=true",
+    demo: "https://notebookshop.netlify.app",
+    github: "https://github.com/fernandobouchet/notebookShop",
+    techstack: ["React", "Bootstrap"],
+  },
+  {
+    id: 5,
+    title: "Image Uploader",
+    info_es:
+      "Aplicación fullstack que permite subir, almacenar una imagen y obtener su url.",
+    info_en:
+      "Full stack application that allows you to upload, store an image and obtain its URL.",
+    image:
+      "https://raw.githubusercontent.com/fernandobouchet/image-uploader/main/project_preview.webp",
+    demo: "http://image-uploader-beryl.vercel.app",
+    github: "https://github.com/fernandobouchet/image-uploader",
+    techstack: ["React", "TypeScript", "Express", "MongoDB", "API"],
+  },
+  {
+    id: 6,
+    title: "Career progress",
+    info_es:
+      "Página web creada para registrar el progreso de mi carrera universitaria.",
+    info_en: "Website created to record the progress of my university career.",
+    image:
+      "https://github.com/fernandobouchet/ProgresoLicenciaturaInformatica/raw/main/progresolicenciatura_preview.webp",
+    demo: "https://progreso-licenciatura-informatica.vercel.app",
+    github:
+      "https://github.com/fernandobouchet/ProgresoLicenciaturaInformatica",
+    techstack: [
+      "React",
+      "Bootstrap",
+      "Firebase",
+      "React Router",
+      "Styled Components",
+    ],
+  },
+  {
+    id: 7,
+    title: "My Unsplash",
+    info_es:
+      "Aplicación donde puedes agregar imagenes (colocando una url y un 'tag'), los datos se almacenan en una base de datos.",
+    info_en:
+      "Application where you can add images (by placing a url and a 'tag'), the data is stored in a database.",
+    image:
+      "https://raw.githubusercontent.com/fernandobouchet/my-unsplash/main/project_preview.webp",
+    demo: "https://my-unsplash-gray.vercel.app",
+    github: "https://github.com/fernandobouchet/my-unsplash",
+    techstack: [
+      "React",
+      "Bootstrap",
+      "TypeScript",
+      "ReactRouter",
+      "Express",
+      "MongoDB",
+      "API",
+    ],
+  },
+  {
+    id: 8,
+    title: "Dev Finder",
+    info_es:
+      "Aplicación que utiliza a API de Github para poder buscar usuarios por su nombre, mostrando su información general.",
+    info_en:
+      "Application that uses the Github API to search for users by name, showing their general information.",
+    image:
+      "https://github.com/fernandobouchet/dev-finder/raw/main/dev-finder_preview.webp",
+    demo: "https://fernandobouchet.github.io/dev-finder",
+    github: "https://github.com/fernandobouchet/dev-finder",
+    techstack: ["React", "API"],
+  },
+  {
+    id: 9,
+    title: "TOP Series",
+    info_es:
+      "Se utilizó la API de 'TMDB' para mostrar información de las series más populares, permite al usuario guardar sus series favoritas y las que desearía 'ver más tarde'.",
+    info_en:
+      "The 'TMDB' API was used to display information on the most popular series, allowing the user to save their favorite series and those they would like to 'watch later'.",
+    image:
+      "https://raw.githubusercontent.com/fernandobouchet/favSeries/main/preview.webp",
+    demo: "https://favseries.netlify.app",
+    github: "https://github.com/fernandobouchet/favSeries",
+    techstack: ["React", "React Router", "Chakra UI", "API"],
+  },
+  {
+    id: 10,
+    title: "Blog .",
+    info_es:
+      'Blog con una página para el "cliente", una página "CMS" para el administrador y una API rest conectada a una base de datos mongoDB.',
+    info_en:
+      'Blog with a page for the "customer", a "CMS" page for the administrator and a rest API connected to a mongoDB database.',
+    image:
+      "https://github.com/fernandobouchet/TOP_Blog-Client/raw/main/project_preview.webp",
+    demo: "https://blog-top.vercel.app",
+    github: "https://github.com/fernandobouchet/TOP_Blog-Client",
+    techstack: [
+      "React",
+      "React Router",
+      "Bootstrap",
+      "Express",
+      "MongoDB",
+      "API",
+    ],
+  },
+  {
+    id: 11,
+    title: "Twitter clone",
+    info_es:
+      "Aplicación web realizada en equipo con otros desarrolladores, utilizando la metodología ágil SCRUM, que emula la experiencia de Twitter, permitiendo a los usuarios registrarse, iniciar sesión, crear perfiles, seguir a otros usuarios y compartir tweets.",
+    info_en:
+      "Web application developed in a team with other developers, using the agile methodology SCRUM, that emulates the Twitter experience, allowing users to register, log in, create profiles, follow other users and share tweets.",
+    image:
+      "https://github.com/fernandobouchet/s8-07-t-node-react-twitter/raw/main/project_preview.webp",
+    demo: "https://s8-07-t-node-react-twitter.vercel.app",
+    github: "https://github.com/fernandobouchet/s8-07-t-node-react-twitter",
+    techstack: [
+      "Next.js",
+      "Tailwind CSS",
+      "React Query",
+      "Express",
+      "MongoDB",
+      "Redux",
+      "API",
+    ],
+  },
+  {
+    id: 12,
+    title: "excel2docxTemplate2pdf",
+    info_es:
+      "Aplicación web que fusiona datos de un archivo XLSX cargado en una plantilla DOCX, permitiendo a los usuarios descargar el resultado en formato DOCX o PDF. Utiliza la API de Gotenberg para la conversión a PDF, alojada en un contenedor Docker.",
+    info_en:
+      "Web application wich merges data from an uploaded XLSX file into a DOCX template, allowing users to download the result as DOCX or PDF. It uses Gotenberg API for PDF conversion, hosted in a Docker container.",
+    image:
+      "https://raw.githubusercontent.com/fernandobouchet/excel2docxTemplate2pdf/main/preview.webp",
+    demo: "https://excel2docx-template2pdf.vercel.app",
+    github: "https://github.com/fernandobouchet/excel2docxTemplate2pdf",
+    techstack: ["Next.js", "Tailwind CSS", "Shadcn/UI", "API"],
+  },
+  {
+    id: 13,
+    title: "FavSongsQuotes",
+    info_es:
+      "Aplicación web que permite descubrir y compartir citas memorables de canciones, con enlaces a los vídeos de las mismas. Ofrece una experiencia interactiva y completamente responsiva para explorar, dar 'me gusta' y gestionar citas de canciones favoritas, usando Supabase como base de datos.",
+    info_en:
+      "Web application that allows users to discover and share memorable song quotes, with links to the videos of the songs. It offers an interactive and fully responsive experience to explore, like, and manage favourite song quotes, using Supabase as the database.",
+    image:
+      "https://raw.githubusercontent.com/fernandobouchet/FavSongQuote/refs/heads/main/preview.webp",
+    demo: "https://favsongsquote.vercel.app",
+    github: "https://github.com/fernandobouchet/FavSongQuote",
+    techstack: [
+      "Next.js",
+      "Tailwind CSS",
+      "Supabase",
+      "Shadcn/UI",
+      "Prisma",
+      "API",
+    ],
+  },
+  {
+    id: 14,
+    title: "TechStore",
+    info_es:
+      "Esta es una aplicación web de un e-commerce ficticio que permite a los usuarios explorar productos, agregar artículos a un carrito y guardar favoritos. La aplicación posee soporte multilenguaje y aprovecha la generación de sitios estáticos (SSG) para un rendimiento óptimo y una carga rápida.",
+    info_en:
+      "This is a fictional e-commerce web application that allows users to browse products, add items to a cart, and save favorites. The application has multi-language support and takes advantage of static site generation (SSG) for optimal performance and fast loading.",
+    image:
+      "https://raw.githubusercontent.com/fernandobouchet/store/refs/heads/main/preview.webp",
+    demo: "https://storetechbeta.vercel.app",
+    github: "https://github.com/fernandobouchet/store",
+    techstack: ["Next.js", "Tailwind CSS", "Next UI", "Next-intl"],
+  },
+  {
+    id: 15,
+    title: "SimpleTaskBoard",
+    info_es:
+      "Un sistema de gestión de tareas con vista de tabla y tablero Kanban (con soporte para arrastrar y soltar), con soporte multilenguaje y visualización de datos.",
+    info_en:
+      "A task management system with a table view and a Kanban board (with drag and drop support), with multi-language support and data visualization.",
+    image:
+      "https://raw.githubusercontent.com/fernandobouchet/tasks/refs/heads/main/preview.webp",
+    demo: "https://simpletaskboard.vercel.app",
+    github: "https://github.com/fernandobouchet/tasks",
+    techstack: ["Next.js", "Tailwind CSS", "Shadcn/UI", "Prisma", "Next-intl"],
+  },
+];
