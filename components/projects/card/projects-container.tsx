89ec313cd543e1973f8b1a5500e7af5612388af3
fernandobouchet <fernandobouchet@gmail.com>
2025-01-22 22:00:25 -0300

    feat: update 'projects-container' with animations on cards reveal

--- components/projects/card/projects-container.tsx
+++ components/projects/card/projects-container.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { ProjectsData } from "@/utils/projects-data";
 import { ProjectCard } from "./project-card";
 import { Button } from "@/components/ui/button";
+import { motion } from "motion/react";
 
 const ProjectsContainer = () => {
   const sortedProjects = ProjectsData.sort((a, b) => b.id - a.id);
@@ -22,13 +23,22 @@ const ProjectsContainer = () => {
   return (
     <div className="flex flex-col justify-center gap-6">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {projects.map((project) => (
-          <ProjectCard key={project.id} project={project} />
+        {projects.map((project, index) => (
+          <motion.div
+            key={project.id}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: (index % 4) * 0.1 }}
+          >
+            <ProjectCard key={project.id} project={project} />{" "}
+          </motion.div>
         ))}
       </div>
-      <Button onClick={handleOnClick} className="w-1/4 mx-auto">
-        Show more
-      </Button>
+      {projects.length < sortedProjects.length && (
+        <Button onClick={handleOnClick} className="w-1/4 mx-auto">
+          Show more
+        </Button>
+      )}
     </div>
   );
 };
