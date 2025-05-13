a13e47ed43d2ba76536963a804d6ec914daa1d98
fernandobouchet <fernandobouchet@gmail.com>
2025-05-13 19:19:05 -0300

    refactor: remove 'useImageIsLoaded' hook since images now loads instantly

--- components/projects/card/card-image.tsx
+++ components/projects/card/card-image.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { MorphingDialogImage } from "@/components/ui/morphing-dialog";
-import useImageIsLoaded from "@/hooks/useImageIsLoaded";
 
 interface Props {
   src: string;
@@ -9,19 +8,12 @@ interface Props {
 }
 
 const CardImage = ({ src, alt, className }: Props) => {
-  const isLoaded = useImageIsLoaded(src);
-
   return (
     <div className="relative w-full h-48 rounded-2xl overflow-hidden">
-      {!isLoaded && (
-        <div className="absolute inset-0 z-10 bg-gray-200 animate-pulse rounded-b-2xl" />
-      )}
       <MorphingDialogImage
         src={src}
         alt={alt}
-        className={`w-full h-full object-cover rounded-b-2xl hover:scale-110 transition duration-500 ${
-          isLoaded ? "opacity-100" : "opacity-0"
-        } ${className}}`}
+        className={`w-full h-full object-cover rounded-b-2xl hover:scale-110 transition duration-500  ${className}}`}
       />
     </div>
   );
