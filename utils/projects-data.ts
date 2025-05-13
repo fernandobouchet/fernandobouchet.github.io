369eeea038913f41ef51673d0cb51dd3e3a2688f
fernandobouchet <fernandobouchet@gmail.com>
2025-05-13 19:18:36 -0300

    feat: host the projects image's in the bundle for faster loading

--- utils/projects-data.ts
+++ utils/projects-data.ts
@@ -6,8 +6,7 @@ export const ProjectsData = [
       "Aplicación fullstack que permite subir, almacenar una imagen y obtener su URL.",
     info_en:
       "Full stack application that allows you to upload, store an image and obtain its URL.",
-    image:
-      "https://raw.githubusercontent.com/fernandobouchet/image-uploader/main/project_preview.webp",
+    image: "/projects-images/image_uploader.webp",
     demo: "http://image-uploader-beryl.vercel.app",
     github: "https://github.com/fernandobouchet/image-uploader",
     techstack: ["React", "TypeScript", "Express", "MongoDB", "API"],
@@ -19,8 +18,7 @@ export const ProjectsData = [
       "Blog con una página para el cliente, una página CMS para el administrador y una API REST conectada a una base de datos MongoDB.",
     info_en:
       "Blog with a customer-facing page, an admin CMS page and a REST API connected to a MongoDB database.",
-    image:
-      "https://github.com/fernandobouchet/TOP_Blog-Client/raw/main/project_preview.webp",
+    image: "/projects-images/blog.webp",
     demo: "https://blog-top.vercel.app",
     github: "https://github.com/fernandobouchet/TOP_Blog-Client",
     techstack: [
@@ -39,8 +37,7 @@ export const ProjectsData = [
       "Aplicación web realizada en equipo con otros desarrolladores, utilizando la metodología ágil SCRUM, que emula la experiencia de Twitter, permitiendo a los usuarios registrarse, iniciar sesión, crear perfiles, seguir a otros usuarios y compartir tweets.",
     info_en:
       "Web application developed in a team with other developers, using the agile methodology SCRUM, that emulates the Twitter experience, allowing users to register, log in, create profiles, follow other users and share tweets.",
-    image:
-      "https://github.com/fernandobouchet/s8-07-t-node-react-twitter/raw/main/project_preview.webp",
+    image: "/projects-images/twitter_clone.webp",
     demo: "https://s8-07-t-node-react-twitter.vercel.app",
     github: "https://github.com/fernandobouchet/s8-07-t-node-react-twitter",
     techstack: [
@@ -60,8 +57,7 @@ export const ProjectsData = [
       "Aplicación web que fusiona datos de un archivo XLSX cargado en una plantilla DOCX, permitiendo a los usuarios descargar el resultado en formato DOCX o PDF. Utiliza la API de Gotenberg para la conversión a PDF, alojada en un contenedor Docker.",
     info_en:
       "Web application which merges data from an uploaded XLSX file into a DOCX template, allowing users to download the result as DOCX or PDF. It uses Gotenberg API for PDF conversion, hosted in a Docker container.",
-    image:
-      "https://raw.githubusercontent.com/fernandobouchet/excel2docxTemplate2pdf/main/preview.webp",
+    image: "/projects-images/excel2docxTamplate2pfd.webp",
     demo: "https://excel2docx-template2pdf.vercel.app",
     github: "https://github.com/fernandobouchet/excel2docxTemplate2pdf",
     techstack: ["Next.js", "Tailwind CSS", "Shadcn/UI", "API"],
@@ -74,8 +70,7 @@ export const ProjectsData = [
       "Una wiki completa y multilingüe sobre The Office, que proporciona información detallada sobre episodios, personajes y más, impulsada por la API de TMDB.",
     info_en:
       "A comprehensive and multilingual wiki for The Office, providing detailed information about episodes, characters, and more, powered by the TMDB API.",
-    image:
-      "https://raw.githubusercontent.com/fernandobouchet/the_office_wiki/refs/heads/main/preview.webp",
+    image: "/projects-images//the_office_wiki.webp",
     demo: "https://officewiki.vercel.app",
     github: "https://github.com/fernandobouchet/the_office_wiki",
     techstack: ["Next.js", "Tailwind CSS", "Shadcn/UI", "Next-intl"],
@@ -87,8 +82,7 @@ export const ProjectsData = [
       "Esta es una aplicación web de e-commerce ficticio que permite a los usuarios explorar productos, agregar artículos a un carrito y guardar favoritos. La aplicación posee soporte multilenguaje y aprovecha la generación de sitios estáticos (SSG) para un rendimiento óptimo y una carga rápida.",
     info_en:
       "This is a fictional e-commerce web application that allows users to browse products, add items to a cart, and save favorites. The application has multi-language support and takes advantage of static site generation (SSG) for optimal performance and fast loading.",
-    image:
-      "https://raw.githubusercontent.com/fernandobouchet/store/refs/heads/main/preview.webp",
+    image: "/projects-images/techstore.webp",
     demo: "https://storetechbeta.vercel.app",
     github: "https://github.com/fernandobouchet/store",
     techstack: ["Next.js", "Tailwind CSS", "Next UI", "Next-intl"],
@@ -100,8 +94,7 @@ export const ProjectsData = [
       "Aplicación web que permite descubrir y compartir citas memorables de canciones, con enlaces a los vídeos de las mismas. Ofrece una experiencia interactiva y completamente responsiva para explorar, dar 'me gusta' y gestionar citas de canciones favoritas, usando Supabase como base de datos.",
     info_en:
       "Web application that allows users to discover and share memorable song quotes, with links to the videos of the songs. It offers an interactive and fully responsive experience to explore, like, and manage favourite song quotes, using Supabase as the database.",
-    image:
-      "https://raw.githubusercontent.com/fernandobouchet/FavSongQuote/refs/heads/main/preview.webp",
+    image: "/projects-images/fav_song_quote.webp",
     demo: "https://favsongsquote.vercel.app",
     github: "https://github.com/fernandobouchet/FavSongQuote",
     techstack: [
@@ -121,8 +114,7 @@ export const ProjectsData = [
       "Un sistema de gestión de tareas con vista de tabla y tablero Kanban (con soporte para arrastrar y soltar), con soporte multilenguaje y visualización de datos.",
     info_en:
       "A task management system with a table view and a Kanban board (with drag and drop support), with multi-language support and data visualization.",
-    image:
-      "https://raw.githubusercontent.com/fernandobouchet/tasks/refs/heads/main/preview.webp",
+    image: "/projects-images/simple_task_board.webp",
     demo: "https://simpletaskboard.vercel.app",
     github: "https://github.com/fernandobouchet/tasks",
     techstack: [
