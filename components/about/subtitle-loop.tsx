64b26afc7775bc3fa94664c1dfc9397cd60b7520
fernandobouchet <fernandobouchet@gmail.com>
2025-06-03 23:30:02 -0300

    feat: add 'subtitle-loop' component

---
+++ components/about/subtitle-loop.tsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { TextLoop } from "../motion-primitives/text-loop";
+import { LanguageContext } from "@/context/language-context";
+
+const SubtitleLoop = () => {
+  const { texts } = useContext(LanguageContext);
+
+  return (
+    <TextLoop className="pb-3 overflow-y-clip">
+      {texts.about.subtitles.map((subtitle, index) => (
+        <span key={index} className="text-lg md:text-2xl font-medium">
+          {subtitle}
+        </span>
+      ))}
+    </TextLoop>
+  );
+};
+
+export { SubtitleLoop };
