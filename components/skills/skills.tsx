84ace111d1dd7df64009378af85d733946577058
fernandobouchet <fernandobouchet@gmail.com>
2025-06-18 22:07:04 -0300

    feat: update 'skills' section

--- components/skills/skills.tsx
+++ components/skills/skills.tsx
@@ -5,8 +5,9 @@ import { SectionContainer } from "../ui/section-container";
 import { LanguageContext } from "@/context/language-context";
 import { SkillCard } from "./skill-card";
 import { HiCode, HiDatabase } from "react-icons/hi";
-import { FiLayers } from "react-icons/fi";
 import { HiWrench } from "react-icons/hi2";
+import { RiPaletteFill } from "react-icons/ri";
+import { BsFillLayersFill } from "react-icons/bs";
 
 const Skills = () => {
   const { texts } = useContext(LanguageContext);
@@ -17,32 +18,37 @@ const Skills = () => {
       title: texts.skills.languages,
       icon: <HiCode />,
       items: [
-        "HTML",
-        "CSS",
-        "Node.js",
         "JavaScript (ES6+)",
         "TypeScript",
         "Java",
         "Python",
+        "HTML",
+        "CSS",
       ],
     },
     {
       id: 2,
-      title: texts.skills.frameworks,
-      icon: <FiLayers />,
-      items: ["React", "Next.js", "Express.js", "Spring Boot", "Tailwind CSS"],
+      title: texts.skills.frontend,
+      icon: <RiPaletteFill />,
+      items: ["React", "Next.js", "Tailwind CSS"],
     },
     {
       id: 3,
-      title: texts.skills.databases,
-      icon: <HiDatabase />,
-      items: ["MongoDB", "PostgreSQL", "MySQL", "Prisma (ORM)"],
+      title: texts.skills.backend,
+      icon: <BsFillLayersFill />,
+      items: ["Node.js", "Express.js", "Spring Boot"],
     },
     {
       id: 4,
+      title: texts.skills.databases,
+      icon: <HiDatabase />,
+      items: ["PostgreSQL", "MongoDB", "MySQL", "Prisma (ORM)"],
+    },
+    {
+      id: 5,
       title: texts.skills.tools,
       icon: <HiWrench />,
-      items: ["Git", "GitHub", "Docker", "Linux", "NPM", "Agile (Scrum)"],
+      items: ["Git", "Docker", "Linux", "Agile", "GitHub"],
     },
   ];
 
@@ -50,7 +56,7 @@ const Skills = () => {
     <SectionContainer sectionId="skills">
       <h2 className="section-title">{texts.skills.title}</h2>
       <p className="text-center pb-4">{texts.skills.p1}</p>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4">
         {SkillsSections.length > 0 &&
           SkillsSections.map((section) => (
             <SkillCard
