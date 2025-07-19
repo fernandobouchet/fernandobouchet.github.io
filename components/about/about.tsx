0d3d88b937111b36fd6f9814063d793fea795678
fernandobouchet <fernandobouchet@gmail.com>
2025-07-19 16:44:40 -0300

    fix: fixed missing translation

--- components/about/about.tsx
+++ components/about/about.tsx
@@ -26,11 +26,10 @@ const About = () => {
           {texts.about.p3}
           <span className="highlighted-text">{texts.about.span3}</span>
           {texts.about.p4}
-          <span className="highlighted-text">{texts.about.span4}</span>
         </p>
         <p>
           {texts.about.p5}
-          <span className="highlighted-text">{texts.about.span5}</span>
+          <span className="highlighted-text">{texts.about.span4}</span>
           {texts.about.p6}
         </p>
       </div>
