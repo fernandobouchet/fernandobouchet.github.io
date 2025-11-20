4bdfc0309237d527f09d810e675ef760477788b1
Fernando Bouchet <fernandobouchet@gmail.com>
2025-11-19 22:27:40 -0300

    feat: update 'background' component

--- components/ui/background.tsx
+++ components/ui/background.tsx
@@ -1,8 +1,10 @@
 const Background = () => {
   return (
-    <div className="fixed inset-0 -z-10 w-full h-full bg-background">
-      <div className="absolute inset-0 bg-[radial-gradient(circle_500px_at_50%_550px,var(--accent),transparent)]" />
+    <div className="fixed inset-0 -z-10 w-full h-full bg-background transition-colors duration-300">
+      <div className="absolute inset-0 bg-[radial-gradient(circle_800px_at_50%_-100px,var(--accent),transparent)] opacity-40 dark:opacity-20" />
+      <div className="absolute bottom-0 right-0 bg-[radial-gradient(circle_600px_at_100%_100%,var(--primary),transparent)] opacity-10 dark:opacity-10" />
     </div>
   );
 };
+
 export { Background };
